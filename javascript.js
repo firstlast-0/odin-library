00e557e3f67ea8aa58d7ddfe5a1062ae887479c6
Vincent <2w121q@gmail.com>
2024-02-29 14:48:30 +0800

    Add the feature to add books

--- javascript.js
+++ javascript.js
@@ -10,10 +10,10 @@ function Book(title, author, pages, read) {
     };
 }
 
-let book1 = new Book('book1', 'auth1', 1, 'read');
-let book2 = new Book('book2', 'auth2', 2, 'not yet read');
-addBookToLibrary(book1);
-addBookToLibrary(book2);
+// let book1 = new Book('book1', 'auth1', 1, 'read');
+// let book2 = new Book('book2', 'auth2', 2, 'not yet read');
+// addBookToLibrary(book1);
+// addBookToLibrary(book2);
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
@@ -33,3 +33,35 @@ function displayLibrary(library) {
     }    
 }
 displayLibrary(myLibrary);
+
+let dialog = document.querySelector('dialog');
+let newBook = document.querySelector('dialog + button');
+let submit = document.querySelector('#sub');
+let close = document.querySelector('#can');
+
+newBook.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+close.addEventListener("click", () => {
+  dialog.close();
+});
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector("input[type='number']").value;
+  let read = (document.querySelector('#read').checked) ? 'Read' : 'Not Yet Read';
+
+  let del = document.createElement('button');
+  del.textContent = 'Delete';
+  del.addEventListener('click', () => {
+    ul.removeChild(li);
+  });
+
+  let book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+  displayLibrary(myLibrary);
+  dialog.close();
+});
